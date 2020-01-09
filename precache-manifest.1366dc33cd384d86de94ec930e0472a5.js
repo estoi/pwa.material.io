5d9d3ac8b3b072d966f4d23448b3619d1aabb3b4
liuguang <liuguang@kayak.com.cn>
2020-01-09 10:15:59 +0800

    Updates

---
+++ precache-manifest.1366dc33cd384d86de94ec930e0472a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac752ff708ab49b88a16a83533f9edd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ee898b5ad952d7ed3589",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c26784c2ae238e56eb74",
+    "url": "./static/js/2.e264b078.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.e264b078.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee898b5ad952d7ed3589",
+    "url": "./static/js/main.07b94ce4.chunk.js"
+  },
+  {
+    "revision": "4d052d63d6384a489a33",
+    "url": "./static/js/runtime-main.dba7f283.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
